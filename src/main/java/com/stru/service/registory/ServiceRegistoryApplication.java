e5957398a16d7d8b2aa88ff28257d52a41e7024a
Rahil Baig <rahil@striketru.com>
2026-02-18 23:38:44 +0530

    added printing statement

--- src/main/java/com/stru/service/registory/ServiceRegistoryApplication.java
+++ src/main/java/com/stru/service/registory/ServiceRegistoryApplication.java
@@ -10,6 +10,7 @@ public class ServiceRegistoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistoryApplication.class, args);
+        System.out.println("test");
 	}
 
 }
